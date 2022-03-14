a5e5bfb3bc5f9459cba60af0cc648d4b485aea25
Yuval Lande <yuvalande@gmail.com>
2022-03-14 11:38:08 +0200

    took down previous help command

--- cogs/general_cog.py
+++ cogs/general_cog.py
@@ -36,24 +36,6 @@ class General(commands.Cog):
     async def on_ready(self):
         print(f"{self.bot.user.name} has connected to Discord!")
 
-    # Embeded help with list and details of commands
-    # @commands.command()
-    # async def help(ctx):
-    #     embed = discord.Embed(color=discord.Colour.green())
-    #     embed.set_author(name="Help : list of commands available")
-    #     embed.add_field(
-    #         name=".ping", value="Returns bot respond time in milliseconds", inline=True
-    #     )
-    #     embed.add_field(
-    #         name=".minyan",
-    #         value="Arrange teams for a 5v5, assuming everyone has a number from 1 to 10",
-    #         inline=False,
-    #     )
-    #     embed.add_field(
-    #         name=".nadav", value="The essence of Nadav", inline=False,
-    #     )
-    #     embed.add_field(name=".oops", value="Send an embarrassing image", inline=False)
-    #     await ctx.send(embed=embed)
 
     # answers with the ms latency
     @commands.command(help="Answers with the ms latency")
